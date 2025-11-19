55bc46ca4a4382c631ef122151970cae5d771a14
iuryliberato <iuryliberato@hotmail.com>
2025-11-19 11:28:43 +0100

    deploy test

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,6 @@
 // netlify/functions/api.js
 const serverless = require('serverless-http');
-const app = require('../../server');
+const app = require('../../app');
 
 module.exports.handler = serverless(app, {
   basePath: '/.netlify/functions/api',
